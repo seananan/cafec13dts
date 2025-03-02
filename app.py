e81f3e46e1dc0864c5179d43912b62be3fb80001
veryl <21236@wc.school.nz>
2025-03-03 07:55:56 +1300

    I added the menu and used database to add items to menu.

--- app.py
+++ app.py
@@ -1,17 +1,41 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import ERROR
+
+DATABASE = "DB_FILE"
 
 app = Flask(__name__)
+
+def connect_database(db_file):
+    """
+    creates a connection to the database
+    :param db_file:
+    :return: conn
+    """
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return
+
 
 
 @app.route('/')
 def render_homepage():  # put application's code here
-    return render_template('home.html'
+    return render_template('home.html')
 
 
-
-
-
-
+@app.route('/menu')
+def render_menu():
+    con = create_database(DATABASE)
+    query = "SELECT name, description, volume, image, price FROM products"
+    cur = con.cursor()
+    cur.execute(query)
+    product_list = cur.fetchall()
+    print(product_list)
+    con.close()
+    return render_template('menu.html', list_of_coffee=product_list)
 
 
 if __name__ == '__main__':
